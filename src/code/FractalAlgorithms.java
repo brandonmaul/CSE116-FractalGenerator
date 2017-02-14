f109f525e31ea4dfdff08996524487b8706c9a1d
Brandon Maul <brandonmaul@users.noreply.github.com>
2017-02-14 13:20:24 -0500

    Added Methods that set Initial Values of Pixels

--- src/code/FractalAlgorithms.java
+++ src/code/FractalAlgorithms.java
@@ -34,5 +34,51 @@ public interface FractalAlgorithms {
 		p.setX(newX); 
 		p.setY(newY);
 	}
+	
+	public default void initMandlebrot(Pixel[][] pixelArray){
+		pixelArray[0][0].setX(-2.15);
+		pixelArray[0][0].setY(-1.3);
+		
+		for(int x=0; x<pixelArray.length; x++){
+			for(int y=0; y<pixelArray[0].length; y++){
+				if(x != 0){
+					pixelArray[x][y].setX((pixelArray[x-1][y] + 0.00537109375));
+				}elif(y !=0){
+					pixelArray[x][y].setY((pixelArray[x][y-1] + 0.005078125));
+				}
+			}
+		}
+	}
+	
+	public default void initJulia(Pixel[][] pixelArray){
+		pixelArray[0][0].setX(-1.7);
+		pixelArray[0][0].setY(-1.0);
+		
+		for(int x=0; x<pixelArray.length; x++){
+			for(int y=0; y<pixelArray[0].length; y++){
+				if(x != 0){
+					pixelArray[x][y].setX((pixelArray[x-1][y] + 0.0052734375));
+				}elif(y !=0){
+					pixelArray[x][y].setY((pixelArray[x][y-1] + 0.00390625));
+				}
+			}
+		}
+	}
+	
+	public default void initBurningShip(Pixel[][] pixelArray){
+		pixelArray[0][0].setX(-1.8);
+		pixelArray[0][0].setY(-0.08);
+		
+		for(int x=0; x<pixelArray.length; x++){
+			for(int y=0; y<pixelArray[0].length; y++){
+				if(x != 0){
+					pixelArray[x][y].setX((pixelArray[x-1][y] + 0.0068359375));
+				}elif(y !=0){
+					pixelArray[x][y].setY((pixelArray[x][y-1] + 0.00020507812));
+				}
+			}
+		}
+	}
+
 
 }
