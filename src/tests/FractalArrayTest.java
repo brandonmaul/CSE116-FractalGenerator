2f777bfc21e664ccd1d833bef635219a14bbf102
ThankamAbish <ThankamAbish@10.84.9.108>
2017-02-18 15:24:43 -0500

    Fractal Array Size Tests

---
+++ src/tests/FractalArrayTest.java
@@ -0,0 +1,43 @@
+package tests;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+import code.FractalGenerator;
+import code.Pixel;
+
+public class FractalArrayTest {
+	/**
+	 * @author ThankamAbish
+	 */
+	@Test
+	public void MBSizeTest() {
+		FractalGenerator fractal = new FractalGenerator();
+		Pixel[][] a = fractal.genMandelbrot();
+		//length of a column
+		assertEquals(512, a[0].length);
+		//length of a row
+		assertEquals(512, a.length);
+	}
+	
+	@Test
+	public void JSizeTest() {
+		FractalGenerator fractal = new FractalGenerator();
+		Pixel[][] a = fractal.genJulia();
+		//length of a column
+		assertEquals(512, a[0].length);
+		//length of a row
+		assertEquals(512, a.length);
+	}
+
+	@Test
+	public void BurnSizeTest() {
+		FractalGenerator fractal = new FractalGenerator();
+		Pixel[][] a = fractal.genBurningShip();
+		//length of a column
+		assertEquals(512, a[0].length);
+		//length of a row
+		assertEquals(512, a.length);
+	}
+}
