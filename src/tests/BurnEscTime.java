8c9430c5204159f77c8898dc19ca46b41c4ee106
giselleash <giselleash@yahoo.com>
2017-02-18 15:00:29 -0500

    Burn Escape Time - 0 or 1

---
+++ src/tests/BurnEscTime.java
@@ -0,0 +1,24 @@
+package tests;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+import code.FractalGenerator;
+	/**
+	 * 
+	 * @author Giselle
+	 *
+	 */
+
+public class BurnEscTime {
+	@Test
+	public void BurnEscTimeTest() {
+		FractalGenerator fractal = new FractalGenerator();
+		
+		fractal.genBurningShip();
+		
+		assertTrue(fractal.boolContainsNoZerosOrOnes());
+		
+	}
+}
