ca4c1841b4a1881617b294bf290d6bc5cff8892f
JeffLandberg <landbergcomputerco@gmail.com>
2017-02-20 09:40:46 -0500

    Update EscTest for last fractal

--- src/tests/EscTimeMaxDist.java
+++ src/tests/EscTimeMaxDist.java
@@ -17,6 +17,12 @@ public class EscTimeMaxDist {
 		FractalGenerator fractal = new FractalGenerator();
 		assertEquals(1,fractal.calcJuliaEscapeTime(2, 1.6933593749999853, 0.9765625),0.01);
 	}
+	
+	@Test
+	public void MultiEscTest() {
+		FractalGenerator fractal = new FractalGenerator();
+		assertEquals(1,fractal.calcMultibrotEscapeTime(2, 0.9921875, 1.05625), 0.01);
+	}
 
 
 }
