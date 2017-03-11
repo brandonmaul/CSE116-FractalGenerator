17c1ef648ebc0e33e71ecfdf4f4b88a025304a05
giselleash <giselleash@yahoo.com>
2017-03-11 13:21:16 -0500

    ESC3

---
+++ src/tests/Esc3Test.java
@@ -0,0 +1,14 @@
+package tests;
+// Escape distance set to 3 after 10 passes of the loop 
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class Esc3Test {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
